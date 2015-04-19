519a7ec67b06fc593291d1bea067b211e80cdf95
Thomas Szteliga <ts@websafe.pl>
2015-04-19 19:32:19 +0200

    Kadu - open source Gadu-Gadu and Jabber/XMPP Instant Messenger

---
+++ Kadu/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'Kadu'
+$registryUninstallerKeyName = 'Kadu'
+$installerType = 'exe'
+$url = 'http://download.kadu.im/stable/windows/Kadu-2.1.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
---
+++ Kadu/tools/chocolateyuninstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'Kadu'
+$registryUninstallerKeyName = 'Kadu'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+$osBitness = Get-ProcessorBits
+ 
+if ($osBitness -eq 64) {
+  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
+} else {
+  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
+} 
+	
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+
