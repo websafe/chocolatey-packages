33534397459fae7b37dd3cce7d2e4f6b9a8a4aa9
Thomas Szteliga <ts@websafe.pl>
2015-04-19 19:35:12 +0200

    hubiC - The OVH cloud storage client

---
+++ hubiC/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'hubiC'
+$registryUninstallerKeyName = '{51fea8cc-5bb6-4312-86f5-1802a10e030d}'
+$installerType = 'exe'
+$url = 'http://mir7.ovh.net/ovh-applications/hubic/hubiC/2.1.1/hubiC-2.1.1.145-windows.exe'
+$silentArgs = '/S /norestart'
+$validExitCodes = @(0,3010)
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
---
+++ hubiC/tools/chocolateyuninstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'hubiC'
+$registryUninstallerKeyName = '{51fea8cc-5bb6-4312-86f5-1802a10e030d}'
+$installerType = 'exe'
+$silentArgs = '/S /norestart'
+$validExitCodes = @(0,3010)
+
+$osBitness = Get-ProcessorBits
+ 
+if ($osBitness -eq 64) {
+  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
+} else {
+  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
+} 
+	
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+
